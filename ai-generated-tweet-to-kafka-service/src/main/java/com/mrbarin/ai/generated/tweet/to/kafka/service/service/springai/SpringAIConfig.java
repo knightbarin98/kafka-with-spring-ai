fb92eea16f10343ad03db54fda829668aac766ff
knightbarin98 <lopezsotoivan01@gmail.com>
2026-01-27 22:07:52 -0600

    add spring ai with open ai

---
+++ ai-generated-tweet-to-kafka-service/src/main/java/com/mrbarin/ai/generated/tweet/to/kafka/service/service/springai/SpringAIConfig.java
@@ -0,0 +1,15 @@
+package com.mrbarin.ai.generated.tweet.to.kafka.service.service.springai;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringAIConfig {
+
+  @Bean
+  ChatClient openAIChatClient(OpenAiChatModel openAiChatModel){
+    return ChatClient.builder(openAiChatModel).build();
+  }
+}
---
+++ ai-generated-tweet-to-kafka-service/src/main/java/com/mrbarin/ai/generated/tweet/to/kafka/service/service/springai/model/User.java
@@ -0,0 +1,4 @@
+package com.mrbarin.ai.generated.tweet.to.kafka.service.service.springai.model;
+
+public record User(Long id) {
+}
